e1a9c6325205a40b2ca7ffcff4109ded89fc5b2b
parsa <parsa@parsas-MacBook-Air.local>
2023-03-01 15:45:34 +0330

    final

--- tamrin/src/component/LastNews.jsx
+++ tamrin/src/component/LastNews.jsx
@@ -2,14 +2,32 @@ import React, { useEffect, useState } from 'react'
 import data from "../datas/dataNews";
 
 const LastNews = () => {
-    const [news, setNews] = useState([]);
+    const [news, setNews] = useState(data);
+    const [internal, setInternal] = useState(data.filter((e) => e.type == "newest" && e.version == 'Internal'));
+    const [Foreign, setForeign] = useState(data.filter((e) => e. type == 'newest' && e.version == 'Foreign' ));
+    const [video , setVideo] = useState(data.filter((e) => e.type == "newest" && e.version  == "video"))
 
     const check = (item) => {
-        console.log(data.filter((e) => e.type == item));
-        setNews(data.filter((e) => e.type == "newest"));
-        console.log(news);
+        if (item == "newest") {
+            document.getElementById('versionContainer').className = "version-container"
+        } else {
+            document.getElementById('versionContainer').classList.add("hide")
+        }
+        setNews(data.filter((e) => e.type == item))
     }
-    
+
+    const versionCheck = (item) => {
+        if (item == 'Internal') {
+            setNews(internal)
+
+        }else if(item == "Foreign"){ 
+            setNews(Foreign)
+        }else if(item == "video"){ 
+            setNews(video)
+        }
+
+    }
+
 
 
 
@@ -20,20 +38,20 @@ const LastNews = () => {
             <div className="btntype-container">
                 <button onClick={() => check("newest")} className="btntype font-Beirut">جدید ترین</button>
                 <button onClick={() => check("mostvisited")} className="btntype font-Beirut">پربازدیدترین</button>
-                <button className="btntype font-Beirut">پربحث ترین </button>
+                <button onClick={() => check("controversial")} className="btntype font-Beirut">پربحث ترین </button>
             </div>
             <div id='versionContainer' className='version-container'>
 
-                <button >
-                    <input type={'checkbox'} id="Internal" />
+                <button  onClick={() => versionCheck("Internal")}>
+                    <input name='filter-radio-btn' type={'radio'} id="Internal" />
                     <label htmlFor="Internal">داخلی</label>
                 </button>
-                <button >
-                    <input type={'checkbox'} id="Foreign" />
+                <button onClick={() => versionCheck("Foreign")}>
+                    <input name='filter-radio-btn' type={'radio'} id="Foreign" />
                     <label htmlFor="Foreign">خارجی</label>
                 </button>
-                <button >
-                    <input type={'checkbox'} id="video" />
+                <button onClick={() => versionCheck('video')}>
+                    <input name='filter-radio-btn' type={'radio'} id="video"  />
                     <label htmlFor="video">ویدیو</label>
                 </button>
 
@@ -41,11 +59,13 @@ const LastNews = () => {
 
             </div>
 
-            {news.map((item) => {
-                return (
-                    <p>{item.msg}</p>
-                )
-            })}
+            <div className='item'>
+                {news.map((item) => {
+                    return (
+                        <p>{item.msg}</p>
+                    )
+                })}
+            </div>
 
         </div>
     )
