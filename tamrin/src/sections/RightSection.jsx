e20927b88ba093946966b1c24a0aca3c6414c034
parsa <parsa@parsas-MacBook-Air.local>
2023-03-06 20:41:49 +0330

    final

--- tamrin/src/sections/RightSection.jsx
+++ tamrin/src/sections/RightSection.jsx
@@ -9,10 +9,12 @@ import HeadSlider from '../component/HeadSlider';
 import Slider3 from '../datas/Slider3';
 import Slider from '../component/Slider';
 import Leagues from '../component/Leagues';
+import League from '../datas/League';
+import PersionLeagues from '../datas/PersionLeagues';
 
 const RightSection = () => {
   const [bigSliderData, setBigSliderData] = useState(BigSliderData)
- 
+
   return (
     <div className='right-section item-radius'>
       {bigSliderData.map((item, itemIndex) => {
@@ -28,12 +30,34 @@ const RightSection = () => {
 
 
           <section className="right-section2">
-          <LeagueofEuropeanNations data1={LeagueAzadegan}  data2={LeagueBartar}/>
+            <LeagueofEuropeanNations data1={LeagueAzadegan} data2={LeagueBartar} />
 
-               
-            
+            <div className='slider'>
+              <HeadSlider />
+              {Slider3.map((item, Itemindex) => {
+                console.log(item);
+                return (
+                  <div>
+                    <Slider item={item} {...item} Itemindex={Itemindex} />
+                  </div>
 
-            <Leagues />
+                )
+              })}
+            </div>
+
+
+
+
+          </section>
+
+
+
+          <section className='right-section1'>
+            <img src="../../public/25.gif" alt="" className='item-radius' />
+
+            <Leagues data={PersionLeagues} name="جدول لیگ های ایران" />
+            <img src={"../../public/31.gif"} alt=""  className='item-radius'/>
+
           </section>
 
         </div>
